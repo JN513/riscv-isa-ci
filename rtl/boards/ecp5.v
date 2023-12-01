a8435c556adff48416225a8b01c1c80350ac7a20
JN513 <jn5.guape@gmail.com>
2023-11-30 21:36:22 -0300

    Atualizando arquivos do auk-v
    Atualizando arquivos do auk-v no config.sh e removendo constraints não
    usadas no top da ecp5 45
    f

--- rtl/boards/ecp5.v
+++ rtl/boards/ecp5.v
@@ -1,19 +1,19 @@
 module top (
     input wire clk25,
-    input wire serial_rx,
-    output wire serial_tx,
-    output wire led_n0,
-    output wire led_n1,
-    output wire led_n2,
-    output wire led_n3,
-    output wire led_n4,
-    output wire led_n5,
-    input wire switch_n0,
+    /*input wire switch_n0,
     input wire switch_n1,
     input wire switch_n2,
     input wire switch_n3,
     input wire switch_n4,
-    input wire switch_n5 // Sistema temporario, futuramente tais pinos podem ser simulados pela serial
+    input wire switch_n5,*/
+    input wire serial_rx,
+    output wire serial_tx,
+    //output wire led_n0,
+    output wire led_n1,
+    /*output wire led_n2,
+    output wire led_n3,
+    output wire led_n4,
+    output wire led_n5*/ // Sistema temporario, futuramente tais pinos podem ser simulados pela serial
 );
     
 core_top #(
@@ -24,9 +24,9 @@ core_top #(
     .clk(clk25),
     .serial_rx(serial_rx),
     .serial_tx(serial_tx),
-    .led_n0(led_n0),
+    //.led_n0(led_n0),
     .led_n1(led_n1),
-    .led_n2(led_n2),
+    /*.led_n2(led_n2),
     .led_n3(led_n3),
     .led_n4(led_n4),
     .led_n5(led_n5),
@@ -34,8 +34,8 @@ core_top #(
     .switch_n1(switch_n1),
     .switch_n2(switch_n2),
     .switch_n3(switch_n3),
-    .switch_n4(switch_n4),
-    .switch_n5(switch_n5)
+    .switch_n4(switch_n4)//,
+    //.switch_n5(switch_n5)*/
 );
 
 endmodule
